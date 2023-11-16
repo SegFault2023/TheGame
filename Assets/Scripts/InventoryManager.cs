b2c1f11067901a8eafc09dbc3cc99893bc1a4ff5
Çağla Nur Yuva <c.yuva2020@gtu.edu.tr>
2023-11-17 02:17:53 +0300

    Line written for debug purposes has been deleted.

--- Assets/Scripts/InventoryManager.cs
+++ Assets/Scripts/InventoryManager.cs
@@ -65,7 +65,6 @@ public class InventoryManager : MonoBehaviour
         {
             if (itemSlot[i].thisItemSelected)
             {
-                Debug.Log(i.ToString());
                 return i;
             }
         }
