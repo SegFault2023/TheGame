20c321d2a0fef49fc7153ea4bd3034e72cf871d1
ayan-furkan <ayan.furkan@outlook.com>
2023-12-19 13:52:49 +0300

    Prefabs and map changes

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,47 +4,32 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    public float smoothing;
     [Header("Following Object")]
     public Transform player;
-    [Header("Boundries")]
-    public float boundX = 0.15f;
-    public float boundY = 0.05f;
+
+
+    [Header("Range")]
+    public Vector2 minPosition;
+    public Vector2 maxPosition;
+
+
+    void Start()
+    {
+        
+    }
 
     // Update is called once per frame
     void LateUpdate()
     {
-        Vector3 delta = Vector3.zero;
-
-        float deltaX = player.position.x - transform.position.x;
-
-        if (deltaX > boundX || deltaX < -boundX)
+        if(transform.position != player.position)
         {
-            if(transform.position.x < player.position.x)
-            {
-                delta.x = deltaX - boundX;
-            }
-            else
-            {
-                delta.x = deltaX + boundX;
-            }
+            Vector3 targetPosition = new Vector3(player.position.x, player.position.y, transform.position.z);
+
+            targetPosition.x = Mathf.Clamp(targetPosition.x, minPosition.x, maxPosition.x);
+            targetPosition.y = Mathf.Clamp(targetPosition.y, minPosition.y, maxPosition.y);
+
+            transform.position = Vector3.Lerp(transform.position, targetPosition, smoothing);
         }
-
-        float deltaY = player.position.y - transform.position.y;
-
-        if(deltaY > boundY || deltaY < -boundY)
-        {
-            if(transform.position.y < player.position.y)
-            {
-                delta.y = deltaY - boundY;
-            }
-            else
-            {
-                delta.y = deltaY + boundY;
-            }
-        }
-
-
-
-        transform.position += new Vector3(delta.x,delta.y,0);
     }
 }
